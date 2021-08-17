e40decbea91a1b195040c7e610011e047b672b89
Daniil <cvb.amaho@gmail.com>
2021-08-17 12:22:08 +0100

    init

---
+++ components/Htag/Htag.tsx
@@ -0,0 +1,22 @@
+import { HtagProps } from './Htag.props';
+import styles from './Htag.module.css';
+
+export const Htag = ({tag, children}: HtagProps): JSX.Element => {
+
+	const h1 = styles.h1;
+	const h2 = styles.h2;
+	const h3 = styles.h3;
+
+
+	switch (tag) {
+		case 'h1':
+			return <h1 className={h1}>{children}</h1>;
+		case 'h2': 
+			return <h2 className={h2}>{children}</h2>;
+		case 'h3':
+			return <h3 className={h3}>{children}</h3>;	
+		default:
+			return <></>;
+	}
+
+};
---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import { Htag, Button, Ptag, Tag, Rating } from '../components';
+import {Layout, withLayout} from '../layout/Layout';
+
+ function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+
+  const [rating, setRating] = useState<number>(4);
+
+
+  return (
+    <>
+      <Htag tag='h1'>{counter}</Htag>
+      <Button appearance='primary' className='asdasd' arrow='right' onClick={() => setCounter(x => x + 1)}>Button</Button>
+      <Button appearance='ghost' arrow='down'>Button</Button>
+      <Ptag size='s'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit saepe esse nobis sequi.</Ptag>
+      <Ptag size='m'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit saepe esse nobis sequi.</Ptag>
+      <Ptag size='l'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi reprehenderit saepe esse nobis sequi.</Ptag>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>red</Tag>
+      <Tag size='s' color='green'>green</Tag>
+      <Tag size='m' color='grey'>grey</Tag>
+      <Tag size='m' color='primary'>primary</Tag>
+      <Rating rating={rating} isEditable setRating={setRating}/>
+    </>
+  );
+}
+
+export default withLayout(Home);
