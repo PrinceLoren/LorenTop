52fed1cf62a675d002b912f579c3ce7b4b7682b9
Daniil <cvb.amaho@gmail.com>
2021-08-20 16:57:08 +0100

    Sort_Reducer

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import { Card, HhData, Htag, Tag } from '../../components';
+import React, { useReducer } from 'react';
+import { Advantages, Card, HhData, Htag, P, Sort, Tag } from '../../components';
 import {TopPageComponentProps} from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '../../interfaces/page.interface';
+import { SortEnum } from '../../components/Sort/Sort.props';
+import { sortReducer } from './sort.reducer';
 
 
 export const TopPageComponent = ({ page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
+	const [{products: sortedProducts, sort}, dispatchSort] = useReducer(sortReducer, {products, sort: SortEnum.Rating});
+
+	const setSort = (sort: SortEnum) => {
+			dispatchSort({type: sort});
+	};
+
+
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.title}>
 				<Htag tag='h1'>{page.title}</Htag>
 				{products && <Tag color='grey'>{products.length}</Tag>}
-				<span>Sort</span>
+				<Sort sort={sort} setSort={setSort}/>
 			</div>
 				<div>
-					{products && products.map(p => (
+					{sortedProducts && sortedProducts.map(p => (
 						<div key={p._id}>
 								{p.title}
 						</div>
@@ -26,8 +35,14 @@ export const TopPageComponent = ({ page, products, firstCategory}: TopPageCompon
 			<Tag color='red' size='m'>hh.ru</Tag>
 			
 			</div>
-				{firstCategory == TopLevelCategory.Courses && <HhData {...page.hh}/>}
-		
+				{firstCategory == TopLevelCategory.Courses && page.hh && <HhData {...page.hh}/>} 
+				{page.advantages && page.advantages.length > 0 && <> 
+					<Htag tag='h2'>Advantages</Htag>
+					<Advantages advantages={page.advantages}/>
+				</>}
+				{page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{__html: page.seoText}}/>}
+					<Htag tag='h2'>Acquired skills</Htag>
+					{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
 		</div>
 	);
 };
